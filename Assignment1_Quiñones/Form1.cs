38f6d8a0cacca2632a52b5c9f09d697c25a89967
Emmanuel John Quiñones <ejquinones2@gmail.com>
2022-04-26 15:11:30 +0800

    first commit

---
+++ Assignment1_Quiñones/Form1.cs
@@ -0,0 +1,25 @@
+namespace Assignment1_Quiñones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Emmanuel John F. Quiñones");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("19 YEARS OLD");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cycling");
+        }
+    }
+}
